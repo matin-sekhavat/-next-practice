6448eda8f1d3cec1d048a84335901f218c13c506
matin-sekhavat <154085450+matin-sekhavat@users.noreply.github.com>
2024-03-26 11:29:31 +0330

    Update push.js

--- pages/push.js
+++ pages/push.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function push() {
-  return <div>push filed</div>;
+  return <div>push filed - now i edit this message in main github editor :))))</div>;
 }
 
 export default push;
